6379898d95e62c708eaac306776a60e62552c24a
Romaric250 <lonfonyuyromeo@gmail.com>
2023-08-26 01:21:09 -1100

    some changes made

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,10 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2], 8)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
